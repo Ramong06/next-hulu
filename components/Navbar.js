589cc1dd6d50aa8d1c04a90b6157b27962fd9dbb
RG-Mac <ramong5406@gmail.com>
2021-10-12 11:12:33 -0500

    set up api info for navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,10 @@
+
+
 const Navbar = () => {
     return (
-        <div>
-            <h1>THIS IS WHERE THE SCROLLING NAVBAR WILL GO</h1>
-        </div>
+        <nav>
+            <div>hello</div>
+        </nav>
     )
 }
 
