022887ee8e428a457dae3f5d47e3473da3c2b1b9
RG-Mac <ramong5406@gmail.com>
2021-10-12 11:34:51 -0500

    navbar returning title props from requests.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,19 @@
+import requests from "../utils/requests";
 
 
 const Navbar = () => {
     return (
         <nav>
-            <div>hello</div>
+            <div>
+                {/* Object.entries() returns an array whose elements are arrays corresponding to the enumerable 
+                string-keyed property [key, value] pairs found directly upon object. The ordering of the properties
+                is the same as that given by looping over the property values of the object manually. */}
+                {/* This part is mapping through the requests to receive the key and value, here the value is being destructured {} 
+                to receive the title and url from the requests.js page */}
+                {Object.entries(requests).map(([key, { title, url }]) => (
+                    <h2>{ title }</h2>
+                ))}
+            </div>
         </nav>
     )
 }
--- utils/requests.js
+++ utils/requests.js
@@ -3,46 +3,57 @@ const API_KEY = process.env.API_KEY;
 export default {
     fetchTrending: {
         title: 'Trending',
+        // using back ticks for URL to interpolate api key into the string.
         url: `trending/all/week?api_key=${API_KEY}&language=en-US`
     },
     fetchTopRated: {
         title: 'Top Rated',
+        // using back ticks for URL to interpolate api key into the string.
         url: `/movie/top_rated?api_key=${API_KEY}&language=en-US`
     },
     fetchActionMovies: {
         title: 'Action',
+        // using back ticks for URL to interpolate api key into the string.
         url: `/discover/movie?api_key=${API_KEY}&with_genres=28`
     },
     fetchComedyMovies: {
         title: 'Comedy',
+        // using back ticks for URL to interpolate api key into the string.
         url: `/discover/movie?api_key=${API_KEY}&with_genres=35`
     },
     fetchHorrorMovies: {
         title: 'Horror',
+        // using back ticks for URL to interpolate api key into the string.
         url: `/discover/movie?api_key=${API_KEY}&with_genres=27`
     },
     fetchRomaceMovies: {
         title: 'Romance',
+        // using back ticks for URL to interpolate api key into the string.
         url: `/discover/movie?api_key=${API_KEY}&with_genres=10749`
     },
     fetchMystery: {
         title: 'Mystery',
+        // using back ticks for URL to interpolate api key into the string.
         url: `/discover/movie?api_key=${API_KEY}&with_genres=9648`
     },
     fetchSciFi: {
         title: 'Sci-Fi',
+        // using back ticks for URL to interpolate api key into the string.
         url: `/discover/movie?api_key=${API_KEY}&with_genres=878`
     },
     fetchWestern: {
         title: 'Western',
+        // using back ticks for URL to interpolate api key into the string.
         url: `/discover/movie?api_key=${API_KEY}&with_genres=37`
     },
     fetchAnimation: {
         title: 'Animation',
+        // using back ticks for URL to interpolate api key into the string.
         url: `/discover/movie?api_key=${API_KEY}&with_genres=16`
     },
     fetchTv: {
         title: 'TV',
+        // using back ticks for URL to interpolate api key into the string.
         url: `/discover/movie?api_key=${API_KEY}&with_genres=10770`
     }
 }
